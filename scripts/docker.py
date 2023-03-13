ed5533963e64f2c0f972ff3986443f397c667b60
Marcos126 <natalemarcos06@gmail.com>
2023-03-13 05:06:14 +0000

    a

--- scripts/docker.py
+++ scripts/docker.py
@@ -3,4 +3,18 @@ import docker
 #Create a Docker client
 client = docker.from_env()
 
+IMAGE = 'environment-mysql:latest'
+
 #Define the container configuration
+container_config = {
+    'image':{IMAGE}
+}
+
+#Create the container
+container = client.containers.create(**container_config)
+
+#Start the container
+container.start()
+
+#Print the container ID
+print(f'Container ID: {container.id}')
