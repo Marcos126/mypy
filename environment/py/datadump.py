89143cace03931ab8cbc2e3d542c1f65f573aace
Marcos126 <natalemarcos06@gmail.com>
2023-03-13 01:57:15 +0000

    it's almost done :D

--- environment/py/datadump.py
+++ environment/py/datadump.py
@@ -2,18 +2,18 @@ import os
 import subprocess
 
 # Set MySQL database credentials
-DB_HOST = 'mysql'
+CT_HOST = 'mysql'
 DB_USER = 'root'
 DB_PASSWORD = 'root'
 DB_NAME = 'lomas'
 
 # Set path to save dump.sql file
-DUMP_PATH = './app/lomas.sql'
+DUMP_PATH = 'lomas.sql'
 
 # Create a mysqldump of the database
-mysqldump_cmd = f"mysqldump -h {DB_HOST} -u {DB_USER} -p{DB_PASSWORD} {DB_NAME} > {DUMP_PATH}"
+mysqldump_cmd = f"mysqldump -h {CT_HOST} -u {DB_USER} -p{DB_PASSWORD} {DB_NAME} > {DUMP_PATH}"
 subprocess.call(mysqldump_cmd, shell=True)
 
 # Copy dump.sql file to the host machine
-docker_cp_cmd = f"docker cp {DB_HOST}:{DUMP_PATH} {os.getcwd()}/dump.sql"
+docker_cp_cmd = f"docker cp {CT_HOST}:{DUMP_PATH} {os.getcwd()}/{DUMP_PATH}"
 subprocess.call(docker_cp_cmd, shell=True)
